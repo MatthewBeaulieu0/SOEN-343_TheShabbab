84492704af6218ed36dbc2d97ef92591da6e802f
Bogdan Podariu <bpodariu@genetec.com>
2022-11-25 00:03:53 -0500

    Email method added
    ***Not working in the repo need to dowload jar file for javax.mail and javax.activation*****

---
+++ src/EmailSystem.java
@@ -0,0 +1,85 @@
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.activation.DataHandler;
+
+
+
+public class EmailSystem {
+
+    
+    public EmailSystem(){
+
+    }
+
+    public void sendEmail(){
+        // Recipient's email ID needs to be mentioned.
+        String to = "matthew.beaulieu631@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "bogdanpoda12@gmail.com";
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication("bogdanpoda12@gmail.com", "123456789");
+
+            }
+
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("Salut mon chummy!!");
+
+            // Now set the actual message
+            message.setText("Ecoute big c'est JouStamina!");
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
+
+
+    }
+
+    
+}
